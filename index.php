5d8c271348fad11c1b716978e4b632c9442d2c04
crdpc <crdpc@yahoo.com.br>
2018-07-11 23:35:10 -0300

    Alteração da linguagem

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
-echo "Suckers and <strong>Fuckers</strong>";
-
+echo "Português";
+echo "Olá <strong>Mundo</strong>";
+echo "English";
+echo "Hello  <strong>World</strong>";
+echo "Germany";
+echo "Hallo <strong>Welt</strong>";
+echo "Netherlands";
+echo "Hallo <strong>wereld</strong>";
 ?>
 
